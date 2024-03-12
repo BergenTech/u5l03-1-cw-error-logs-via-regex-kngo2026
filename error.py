26e06838b930e5be9fd649017336fe2fa7c3f7a9
kngo2026 <148019881+kngo2026@users.noreply.github.com>
2024-03-12 13:05:32 +0000

    assignment - incomplete

--- error.py
+++ error.py
@@ -1,1 +1,14 @@
-
+import json, csv, re
+file_path = "logs.json"
+file_path2 = "error_logs.csv"
+with open(file_path, 'r') as file:
+    content = json.load(file)
+    for i in content:
+        pattern = r"ERROR"
+        string = i["level"]
+        match = re.findall(pattern, string, re.IGNORECASE)
+    with open(file_path2, 'w') as newFile:
+        header = content[0].keys()
+        dictionary = {}
+        for x,y in enumerate(content):
+            print(f"{x}: {y}")
